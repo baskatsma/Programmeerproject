1a581dfcda0b14d2ee955454d36af33f66789574
Bas Katsma <bas.katsma@student.uva.nl>
2018-06-14 21:52:21 +0200

    qegohuwroifdfs

--- js/line.js
+++ js/line.js
@@ -82,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var lineOpacity = "0.25";
         var lineOpacityHover = "0.85";
         var otherLinesOpacityHover = "0.1";
-        var lineStroke = "1.5px";
+        var lineStroke = "2.5px";
         var lineStrokeHover = "2.5px";
 
         var circleOpacity = '0.85';
@@ -99,21 +99,59 @@ document.addEventListener("DOMContentLoaded", function() {
 
         /* Add line into SVG */
         var line = d3.line()
-          .x(d => x(d.year))
-          .y(d => y(d.production));
+          .x(function(d) { console.log(d); return x(d.year) })
+          .y(function(d) { console.log(d); return y(d.production) });
+          // .x(d => x(d.year))
+          // .y(d => y(d.production));
 
         var lines = svg.append('g')
           .attr('class', 'lines');
+
+        // console.log(currentGEOData);
+        // console.log(data);
 
         lines.selectAll('.line-group')
           .data(data).enter()
           .append('g')
           .attr('class', 'line-group')
           .append('path')
+          .on("mouseover", function(d, i) {
+              svg.append("text")
+                .attr("class", "title-text")
+                .style("fill", color(i))
+                .text(d.GEO_TIME)
+                .attr("text-anchor", "middle")
+                .attr("x", lineWidth/2)
+                .attr("y", 5);
+            })
+          .on("mouseout", function(d) {
+              svg.select(".title-text").remove();
+            })
           .attr('class', 'line')
           .attr('d', d => line(d.values))
           .style('stroke', (d, i) => color(i))
-          .style('opacity', lineOpacity);
+          .style('opacity', lineOpacity)
+          .on("mouseover", function(d) {
+              console.log(d);
+              // d3.selectAll('.line')
+        			// 		.style('opacity', otherLinesOpacityHover);
+              // d3.selectAll('.circle')
+        			// 		.style('opacity', circleOpacityOnLineHover);
+              // d3.select(this)
+              //   .style('opacity', lineOpacityHover)
+              //   .style("stroke-width", lineStrokeHover)
+              //   .style("cursor", "pointer");
+            })
+          .on("mouseout", function(d) {
+              console.log(d);
+              // d3.selectAll(".line")
+        			// 		.style('opacity', lineOpacity);
+              // d3.selectAll('.circle')
+        			// 		.style('opacity', circleOpacity);
+              // d3.select(this)
+              //   .style("stroke-width", lineStroke)
+              //   .style("cursor", "none");
+            });
 
         /* Add circles in the line */
         lines.selectAll("circle-group")
@@ -124,6 +162,24 @@ document.addEventListener("DOMContentLoaded", function() {
           .data(d => d.values).enter()
           .append("g")
           .attr("class", "circle")
+          .on("mouseover", function(d) {
+              console.log(d);
+              // d3.select(this)
+              //   .style("cursor", "pointer")
+              //   .append("text")
+              //   .attr("class", "text")
+              //   .text(d.production)
+              //   .attr("x", d => x(d.year) + 5)
+              //   .attr("y", d => y(d.production) - 10);
+            })
+          .on("mouseout", function(d) {
+              console.log(d);
+              // d3.select(this)
+              //   .style("cursor", "none")
+              //   .transition()
+              //   .duration(duration)
+              //   .selectAll(".text").remove();
+            })
           .append("circle")
           .attr("cx", d => x(d.year))
           .attr("cy", d => y(d.production))
